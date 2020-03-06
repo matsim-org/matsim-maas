c1133c689c8e3937ad5a0f400de34ccd1b08f57d
leich <leich@vsp.tu-berlin.de>
2020-03-06 17:00:29 +0100

    fix name

--- src/test/java/org/matsim/maas/drt/RunStopBasedDrtExampleTest.java
+++ src/test/java/org/matsim/maas/drt/RunStopBasedDrtExampleTest.java
@@ -26,7 +26,7 @@ import org.junit.Test;
 public class RunStopBasedDrtExampleTest {
 
     @Test
-    public void testDrtDoorToDoor() {
+    public void testDrtStopBased() {
 		RunStopBasedDrtExample.main(new String[] { "" });
     }
 }
