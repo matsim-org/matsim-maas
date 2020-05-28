da28175dd6f442232991c6f7285c58040170cc5c
tschlenther <schlenther@vsp.tu-berlin.de>
2020-05-28 11:55:16 +0200

    GUI and run class for teaching

---
+++ src/main/java/org/matsim/maas/drt/RunDRTClass.java
@@ -0,0 +1,57 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * Controler.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.maas.drt;
+
+import org.matsim.contrib.av.robotaxi.fares.drt.DrtFareModule;
+import org.matsim.contrib.av.robotaxi.fares.drt.DrtFaresConfigGroup;
+import org.matsim.contrib.drt.run.DrtConfigGroup;
+import org.matsim.contrib.drt.run.DrtControlerCreator;
+import org.matsim.contrib.drt.run.MultiModeDrtConfigGroup;
+import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigGroup;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.controler.Controler;
+import org.matsim.vis.otfvis.OTFVisConfigGroup;
+
+public class RunDRTClass {
+
+	public static void run(Config config, boolean otfvis) {
+		Controler controler = DrtControlerCreator.createControlerWithSingleModeDrt(config, otfvis);
+		controler.addOverridingModule(new DrtFareModule());
+		final SimpleVKTCounter vktCounter = new SimpleVKTCounter();
+		controler.addOverridingModule(new AbstractModule() {
+			public void install() {
+				this.addEventHandlerBinding().toInstance(vktCounter);
+			}
+		});
+		controler.run();
+		System.out.println("VKT traveled in last iteration: " + Math.round(vktCounter.getVkt_counted()) + " km");
+	}
+
+	public static void main(String[] args) {
+		Config config = ConfigUtils.loadConfig(args[0], new ConfigGroup[]{new MultiModeDrtConfigGroup(), new DvrpConfigGroup(), new OTFVisConfigGroup(), new DrtFaresConfigGroup()});
+
+		run(config, false);
+	}
+
+}
---
+++ src/main/java/org/matsim/maas/drt/SimpleVKTCounter.java
@@ -0,0 +1,49 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * Controler.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.maas.drt;
+
+import org.matsim.api.core.v01.events.LinkEnterEvent;
+import org.matsim.api.core.v01.events.handler.LinkEnterEventHandler;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+
+import javax.inject.Inject;
+
+public class SimpleVKTCounter implements LinkEnterEventHandler {
+	@Inject
+	Network network;
+	private double vkt_counted = 0.0D;
+
+	SimpleVKTCounter() {
+	}
+
+	public void handleEvent(LinkEnterEvent event) {
+		this.vkt_counted += ((Link)this.network.getLinks().get(event.getLinkId())).getLength() / 1000.0D;
+	}
+
+	public void reset(int iteration) {
+		this.vkt_counted = 0.0D;
+	}
+
+	public double getVkt_counted() {
+		return this.vkt_counted;
+	}
+}
